d24c35a6da6f5c639b5d34916889cf2df9919875
Saurabh Tambe <Saurabh Tambe@DESKTOP-A3G9EUI>
2020-09-06 18:16:25 +0530

    Changes Done..

--- ProductManagament/src/com/exception/ProductNotFoundException.java
+++ ProductManagament/src/com/exception/ProductNotFoundException.java
@@ -1,5 +1,24 @@
 package com.exception;
 
-public class ProductNotFoundException {
+ 
+
+public class ProductNotFoundException extends Exception {
+	
+private String message;
+	
+	public ProductNotFoundException() {
+		// TODO Auto-generated constructor stub
+	}
+	public ProductNotFoundException(String message) {
+		// TODO Auto-generated constructor stub
+		super(message);
+		this.message = message;
+		
+	}
+	
+	public void displayMessage() {
+		System.out.println(getMessage());
+	}
+	
 
 }
